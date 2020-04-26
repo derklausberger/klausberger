0977eed2e8e2e05a39bcfb46aaf0afd2e3a07b66
derklausberger <niklasklausberger@gmail.com>
2020-04-26 22:43:16 +0200

    add methods: get_name, print to class user; mofify enum Action

--- include/user.h
+++ include/user.h
@@ -1,14 +1,22 @@
 #ifndef USER_H
 #define USER_H
 
-enum Action {read,write,execute,delete};
+#include <string>
+
+// erster Buchstabe ist groß, weil delete ein Schlüsselwort ist und
+// dann nicht verwendet werden kann (bzw. ich weiß nicht wie)
+enum class Action {Read,Write,Execute,Delete};
 
 class User {
 private:
-    string name;
-    string pw;
+    std::string name;
+    std::string pw;
 public:
-    User (string _name, string _pw) : name{_name}, pw{_pw} {}
-}
+    User (std::string _name, std::string _pw) : name{_name}, pw{_pw} {}
+
+    std::string get_name();
+
+    void print();
+};
 
 #endif /* end of include guard:  */
---
+++ src/user.cpp
@@ -0,0 +1,14 @@
+#include "user.h"
+
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+string User::get_name() {
+
+}
+
+void User::print() {
+    cout << name << endl;
+}
